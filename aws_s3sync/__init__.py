a30a0ad3ed8339de726f86304290883040f91ed9
Mikhail Advani <mikhail.advani@gmail.com>
2017-04-19 21:30:23 +0530

    Sync to S3

---
+++ aws_s3sync/__init__.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+import os, sys
+import math
+import boto
+import argparse
+import hashlib
+
+AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-b", "--bucket", type=str, required=True, help="Selects the S3 bucket to upload data to")
+    parser.add_argument("-f", "--file_path", type=str, required=True, help="Path of the file to be uploaded")
+    parser.add_argument("-k", "--key", type=str, default=None, help="Key of the object. Same as file_path is undefined")
+    parser.add_argument("-m","--mode", default='auto', choices=['auto', 'sync', 'simple-upload'],help="Method of upload")
+    parser.add_argument("--chunk_size", default=5, type=int, help="Size of chunk in multi-part upload in MB")
+    parser.add_argument("--multipart_threshold", default=10, type=int, help="Minimum size in MB to upload using multipart")
+    args = parser.parse_args()
+
+    if args.key is None:
+        args.key = args.file_path
+    args.chunk_size = args.chunk_size * 1024 *1024
+    args.multipart_threshold = args.multipart_threshold * 1024 *1024
+    return args
+
+def multipart_upload_to_be_used(file_path):
+    file_size = os.stat(file_path).st_size
+    return file_size > args.multipart_threshold
+
+def need_to_update(s3_connection, bucket_name, file_path, s3_path):
+    bucket = s3_connection.get_bucket(bucket_name)
+    key = bucket.get_key(s3_path)
+    if key is None:
+        return True
+    else:
+        local_md5 = hashlib.md5(open(file_path, "rb").read()).hexdigest()
+        return local_md5 != key.etag.strip('"')
+
+def simple_upload(s3_connection, bucket_name, file_path, s3_path):
+    bucket = s3_connection.get_bucket(bucket_name)
+    key = boto.s3.key.Key(bucket, s3_path)
+    try:
+        key.set_contents_from_filename(file_path)
+        print "Upload completed successfully"
+    except Exception as e:
+        print "Upload failed"
+        print e.message
+
+def multipart_upload(s3, bucketname, file_path, s3_path):
+    bucket = s3.get_bucket(bucketname)
+    multipart_upload_request = bucket.initiate_multipart_upload(s3_path)
+
+    file_size = os.stat(file_path).st_size
+    chunks_count = int(math.ceil(file_size / float(args.chunk_size)))
+
+    for i in range(chunks_count):
+        offset = i * args.chunk_size
+        remaining_bytes = file_size - offset
+        payload_bytes = min([args.chunk_size, remaining_bytes])
+        part_num = i + 1
+
+        print "Uploading %d/%d" % (part_num, chunks_count)
+
+        with open(file_path, 'r') as file_pointer:
+            file_pointer.seek(offset)
+            try:
+                multipart_upload_request.upload_part_from_file(fp=file_pointer, part_num=part_num, size=payload_bytes)
+                print ""
+            except Exception as e:
+                multipart_upload_request.cancel_upload()
+                print "Upload failed"
+                print e.message
+
+
+    if len(multipart_upload_request.get_all_parts()) == chunks_count:
+        multipart_upload_request.complete_upload()
+        print "Upload completed successfully"
+    else:
+        multipart_upload_request.cancel_upload()
+        print "Upload failed"
+
+def upload(s3_connection, bucketname, file_path, s3_path):
+    if multipart_upload_to_be_used(args.file_path) and args.mode != 'simple-upload':
+        multipart_upload(s3_connection, bucketname, file_path, s3_path)
+    else:
+        simple_upload(s3_connection, bucketname, file_path, s3_path)
+
+def sync_to_s3():
+    args = parse_arguments()
+    s3_connection = boto.connect_s3(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY)
+    if args.mode != 'sync' or need_to_update(s3_connection, args.bucket, args.file_path, args.key):
+        upload(s3_connection, args.bucket, args.file_path, args.key)
+    else:
+        print "Nothing to update"
+
+
---
+++ setup.py
@@ -0,0 +1,80 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='aws_s3sync',
+
+    version='1.0.0.dev1',
+
+    description='A utility to sync data to/from AWS S3',
+    long_description=long_description,
+
+    url='https://github.com/mikhailadvani/s3sync',
+
+    author='Mikhail Advani',
+    author_email='mikhail.advani@gmail.com',
+
+    license='Apache',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Topic :: System :: Archiving :: Mirroring',
+
+        'License :: OSI Approved :: Apache Software License',
+
+        'Programming Language :: Python :: 2.7'
+    ],
+
+    # What does your project relate to?
+    keywords='aws s3',
+
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages().
+    packages=find_packages(),
+
+    # Alternatively, if you want to distribute just a my_module.py, uncomment
+    # this:
+    #   py_modules=["my_module"],
+
+    # List run-time dependencies here.  These will be installed by pip when
+    # your project is installed. For an analysis of "install_requires" vs pip's
+    # requirements files see:
+    # https://packaging.python.org/en/latest/requirements.html
+    install_requires=['boto'],
+
+    # List additional groups of dependencies here (e.g. development
+    # dependencies). You can install these using the following syntax,
+    # for example:
+    # $ pip install -e .[dev,test]
+    extras_require={},
+
+    # If there are data files included in your packages that need to be
+    # installed, specify them here.  If using Python 2.6 or less, then these
+    # have to be included in MANIFEST.in as well.
+    package_data={},
+
+    # Although 'package_data' is the preferred approach, in some case you may
+    # need to place data files outside of your packages. See:
+    # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
+    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
+    data_files=[],
+
+    # To provide executable scripts, use entry points in preference to the
+    # "scripts" keyword. Entry points provide cross-platform support and allow
+    # pip to create the appropriate form of executable for the target platform.
+    entry_points={
+        'console_scripts': [
+            'sync_to_s3=aws_s3sync:sync_to_s3',
+        ],
+    },
+)
